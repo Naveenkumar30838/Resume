6a23a106a90b8d8a6916497eef2f8f2120616145
Naveen <119968502+Naveenkumar30838@users.noreply.github.com>
2023-09-23 16:35:16 +0530

    add playbase.js
    Added the files which contains all the songs which i have

---
+++ Spotify/playbase.js
@@ -0,0 +1,27 @@
+const playbase =[
+    // Song Name Url Duration 
+    ["Challeya" , "Files/Chaleya.mp3" ,"03:20"],
+    ["Mein Dil Laya" , "Files/MeinDilLaya.mp3" ,"02:46"], 
+    ["Brown_munde" , "Files/Brown_Munde.mp3" ,"04:27"],
+    ["Qara" , "Files/Qara.mp3" ,"00:30"], 
+    ["Illegal Weapon" , "Files/illegalWeapon.mp3" ,"03:08"], 
+    ["Mehbooba" , "Files/Mehbooba.mp3" ,"03:38"], 
+    ["California" , "Files/california.mp3" ,"02:55"], 
+    ["Mere Raske Qamar" , "Files/Mere Raske Qamar.mp3" ,"03:40"], 
+    ["Dus Numbari" , "Files/dus_Numberi.mp3" ,"02:25"], 
+    ["Long Drive" , "Files/Long_drive.mp3" ,"02:30"], 
+    ["Lahore" , "Files/Lahore (1).mp3" ,"03:17"], 
+    ["Tere Naam" , "Files/terenaam.mp3" ,"06:35"], 
+    ["Birthday" , "Files/Birthday.mp3" ,"03:58"], 
+    ["Heartless" , "Files/Heartless.mp3" ,"07:22"], 
+    ["Mere Warga" , "Files/MereWarga.mp3" ,"04:45"], 
+    ["Parinday" , "Files/parinday.mp3" ,"04:23"], 
+    ["Ranjha" , "Files/Ranjha.mp3" ,"03:50"], 
+    ["Raatan Lambiya" , "Files/Raataan Lambiyan .mp3" ,"03:53"], 
+    ["Yaar Purane" , "Files/Yaarpuran.mp3" ,"04:52"], 
+    ["Yalgaar" , "Files/Yalgaar.mp3" ,"03:15"], 
+    ["Tu hai Ki Nahi" , "Files/Tu_Hai_Ki_Nahi.mp3" ,"05:34"],   
+]
+export function allSongs() {
+    return playbase;
+}
