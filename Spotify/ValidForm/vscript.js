370d0bde29be43b5d7f4ddcb1f802c6be661e51b
Naveen <119968502+Naveenkumar30838@users.noreply.github.com>
2023-09-30 16:35:17 +0530

    Update vscript.js

--- Spotify/ValidForm/vscript.js
+++ Spotify/ValidForm/vscript.js
@@ -3,21 +3,21 @@ const playlist =[
     // Song Name Url Duration 
     {name: "Challeya" ,src:"Files/Chaleya.mp3" , duration: "03:20" , singer: ""},  
     {name: "California" , src: "Files/california.mp3" ,duration: "02:55" , singer: ""}, 
-    {name: "Mere Raske Qamar" , src: "Files/Mere Raske Qamar.mp3" ,duration:"03:40" , singer: ""}, 
-    {name: "Dus Numbari" , src: "Files/dus_Numberi.mp3" ,duration:"02:25" , singer: ""}, 
-    {name: "Long Drive" , src: "Files/Long_drive.mp3" ,duration: "02:30" , singer: ""}, 
+   // {name: "Raske Qamar" , src: "Files/Mere Raske Qamar.mp3" ,duration:"03:40" , singer: ""}, 
+   // {name: "Dus Numbari" , src: "Files/dus_Numberi.mp3" ,duration:"02:25" , singer: ""}, 
+  //  {name: "Long Drive" , src: "Files/Long_drive.mp3" ,duration: "02:30" , singer: ""}, 
     {name: "Lahore" , src: "Files/Lahore (1).mp3" ,duration: "03:17",singer:""}, 
+    {name: "Parshawan" , src: "Files/Parshawan.mp3" ,duration: "02:55" ,singer:"Harnoor"}, 
     {name: "Tere Naam" , src: "Files/terenaam.mp3" ,duration:"06:35",singer:""}, 
     {name: "Birthday" , src: "Files/Birthday.mp3" ,duration: "03:58",singer:""}, 
     {name: "Heartless" , src: "Files/Heartless.mp3" ,duration: "07:22" , singer:"Badshah"}, 
     {name: "Mere Warga" , src: "Files/MereWarga.mp3" ,duration: "04:45" ,singer:""}, 
     {name: "Yaar Purane" , src: "Files/Yaarpuran.mp3" ,duration: "04:52" ,singer:""}, 
     {name: "Tu hai Ki Nahi" , src: "Files/Tu_Hai_Ki_Nahi.mp3" ,duration: "05:34" ,singer:""},
-    {name: "Yalgaar" , src: "Files/Yalgaar.mp3" ,duration: "03:15" ,singer:""}, 
-    {name: "Shaam-Lofi" , src: "Files/shaam.mp3" ,duration: "06:08" ,singer:""}, 
     {name: "ChildHood" , src: "Files/Childhood.mp3" ,duration: "02:04" ,singer:""}, 
+  //  {name: "Yalgaar" , src: "Files/Yalgaar.mp3" ,duration: "03:15" ,singer:""}, 
+    {name: "Shaam-Lofi" , src: "Files/shaam.mp3" ,duration: "06:08" ,singer:""}, 
     {name: "Chan Vekhya" , src: "Files/chanVekhya.mp3" ,duration: "03:30" ,singer:"Harnoor"}, 
-    {name: "Parshawan" , src: "Files/Parshawan.mp3" ,duration: "02:55" ,singer:"Harnoor"}, 
 ]
 export function orgsongListFun() {
  return playlist;
