2c3a66341882bd84870824bd3a9f6ea75be18076
Naveen <119968502+Naveenkumar30838@users.noreply.github.com>
2023-10-07 15:36:09 +0530

    Update playbase.js

--- Spotify/playbase.js
+++ Spotify/playbase.js
@@ -1,26 +1,18 @@
 const playbase =[
     // Song Name Url Duration 
-    ["Challeya" , "Files/Chaleya.mp3" ,"03:20"],
-    ["Mein Dil Laya" , "Files/MeinDilLaya.mp3" ,"02:46"], 
-    ["Brown_munde" , "Files/Brown_Munde.mp3" ,"04:27"],
-    ["Qara" , "Files/Qara.mp3" ,"00:30"], 
-    ["Illegal Weapon" , "Files/illegalWeapon.mp3" ,"03:08"], 
-    ["Mehbooba" , "Files/Mehbooba.mp3" ,"03:38"], 
-    ["California" , "Files/california.mp3" ,"02:55"], 
-    ["Mere Raske Qamar" , "Files/Mere Raske Qamar.mp3" ,"03:40"], 
-    ["Dus Numbari" , "Files/dus_Numberi.mp3" ,"02:25"], 
-    ["Long Drive" , "Files/Long_drive.mp3" ,"02:30"], 
-    ["Lahore" , "Files/Lahore (1).mp3" ,"03:17"], 
-    ["Tere Naam" , "Files/terenaam.mp3" ,"06:35"], 
-    ["Birthday" , "Files/Birthday.mp3" ,"03:58"], 
-    ["Heartless" , "Files/Heartless.mp3" ,"07:22"], 
-    ["Mere Warga" , "Files/MereWarga.mp3" ,"04:45"], 
-    ["Parinday" , "Files/parinday.mp3" ,"04:23"], 
-    ["Ranjha" , "Files/Ranjha.mp3" ,"03:50"], 
-    ["Raatan Lambiya" , "Files/Raataan Lambiyan .mp3" ,"03:53"], 
-    ["Yaar Purane" , "Files/Yaarpuran.mp3" ,"04:52"], 
-    ["Yalgaar" , "Files/Yalgaar.mp3" ,"03:15"], 
-    ["Tu hai Ki Nahi" , "Files/Tu_Hai_Ki_Nahi.mp3" ,"05:34"],   
+    {name: "Challeya" ,src:"Files/Chaleya.mp3" , duration: "03:20" },  
+    {name: "Lahore" , src: "Files/Lahore (1).mp3" ,duration: "03:17",singer:""}, 
+    {name: "Shaam-Lofi" , src: "Files/shaam.mp3" ,duration: "06:08" ,singer:""}, 
+    {name: "Parshawan" , src: "Files/Parshawan.mp3" ,duration: "02:55" ,singer:"Harnoor"}, 
+    {name: "Tu hai Ki Nahi" , src: "Files/Tu_Hai_Ki_Nahi.mp3" ,duration: "05:34" ,singer:""},
+    {name:"Brown Munde" , src: "Files/Brown_Munde.mp3" ,duration:"04:27"},
+    {name:"Khaab" , src: "Files/Khaab.mp3",duration:"03:21"},
+    {name:"Zehra" , src: "Files/Zehra.mp3",duration:"03:48"},
+    
+    {name:"Random" , src: "Files/videoplayback.mp3",duration:"03:13"},
+    {name:"Ranjha" , src: "Files/Ranjha.mp3",duration:"03:50"},
+    {name:"Raatan Lambiya" , src: "Files/Raataan Lambiyan .mp3",duration:"03:50"},
+
 ]
 export function allSongs() {
     return playbase;
